4bf7b1a4611b8105c91d16d2c86931c59bc3defe
danielbelle <henrique.danielb@gmail.com>
2024-05-30 14:04:36 -0300

    add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello from Stopwatch
+    <div className="app">
+      <header className="container">
+        <h1 className="center">
+          Stopwatch
+        </h1>
+        <Timer />
+
       </header>
     </div>
   );
 }
 
-export default App;
+function Timer() {
+  const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+
+  function handleStart() {
+    setIsRunning(!isRunning)
+  }
+
+  function handleStop() {
+    setIsRunning(false)
+    setTimer(0)
+  }
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+
+      intervalId = setInterval(() => setTimer(timer + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, timer]);
+
+  // Hours calculation
+  const hours = Math.floor(timer / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((timer % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((timer % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = timer % 100;
+
+  return (
+    <>
+      <p className='center'>{hours}:{minutes}:{seconds}:{milliseconds}</p>
+      <Button onClick={handleStart}>{isRunning ? "Pause" : "Start"}</Button>
+      <Button onClick={handleStop} className="btn-outline">Reset</Button>
+    </>
+  )
+}
+
+function Button({ children, className, onClick }) {
+  return <button className={`btn ${className}`} onClick={onClick}>{children}</button>
+}
